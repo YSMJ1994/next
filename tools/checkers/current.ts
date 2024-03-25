b5c3655e8a6dd2ffd4f27171527a3d44a62f55aa
珵之 <chengzhi.zpc@alibaba-inc.com>
2024-03-25 14:21:37 +0800

    test(*): t111

--- tools/checkers/current.ts
+++ tools/checkers/current.ts
@@ -6,26 +6,41 @@
 ------------------------------------------------------------
 */
 import fs from 'fs-extra';
-import path from 'path';
-import { querySync, registryTask, SRC_DIR_PATH } from '../utils';
+import { escapeRegExp, camelCase, kebabCase } from 'lodash';
+import { getComponentName, querySync, execSync, registryTask, SRC_DIR_PATH } from '../utils';
 
 registryTask(__filename, 'check:current', () => {
-    const branchName = querySync('git', ['branch', '--show-current']);
-    console.log('[ branch ]', branchName);
-    const logs = querySync('git', ['log', '-1', '--format=%s']);
-    console.log('[ logs ]', logs);
-    
     const prBaseSha = fs.readFileSync('/tmp/pr_base', 'utf-8').replace(/^[\n\s]+|[\n\s]+$/g, '');
-    console.log('[ prBaseSha ]', prBaseSha);
-    const commits = querySync('git', ['log', `-20`, '--format=%s'])
-    console.log('[ commits ]', commits);
-    // const GITHUB_OUTPUT_FILE = process.env.GITHUB_OUTPUT as string;
-    // const text = fs.readFileSync(GITHUB_OUTPUT_FILE, 'utf-8');
-    // console.log('[ text ]', text);
-    // const children = fs.readdirSync(SRC_DIR_PATH);
-    // const current = children.find(dir => {
-    //   // if (re) {
+    const commits = querySync('git', ['log', `${prBaseSha}...HEAD`, '--format=%s']);
 
-    //   // }
-    // })
+    const children = fs.readdirSync(SRC_DIR_PATH);
+    const namesMap = new Map(
+        children
+            .map(name => {
+                return [
+                    [name, name],
+                    [camelCase(name), name],
+                    [getComponentName(name), name],
+                    [kebabCase(name), name],
+                ] as Array<[string, string]>;
+            })
+            .flat()
+    );
+    const reg = new RegExp(
+        `\\((${children
+            .map(name => {
+                return [name, camelCase(name), getComponentName(name), kebabCase(name)]
+                    .map(t => escapeRegExp(t))
+                    .join('|');
+            })
+            .join('|')})\\)`,
+        'g'
+    );
+    const matched = commits.match(reg);
+    if (!matched) {
+        return;
+    }
+    const names = Array.from(new Set(matched)).map(t => t.replace(/^\(|\)$/g, ''));
+    const targetNames = names.map(name => namesMap.get(name)!);
+    execSync('npm', ['run', 'check', ...targetNames]);
 });
